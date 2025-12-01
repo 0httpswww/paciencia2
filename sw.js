6081afc7472ca371f0a1948abd35994898846b0e
0httpswww <growerbrdw@gmail.com>
2025-12-01 11:13:46 -0300

    feat: Update version and improve service worker
    Updates the application version to v4.5.1 and enhances the service worker with basic caching for offline access and improved AdSense script loading.

--- sw.js
+++ sw.js
@@ -3,4 +3,34 @@ self.options = {
     "zoneId": 10234210
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+try {
+    importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+} catch (e) {
+    console.log("Ad script import failed", e);
+}
+
+const CACHE_NAME = 'jogos-online-v1';
+const URLS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(URLS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
